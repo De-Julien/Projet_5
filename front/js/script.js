484952fda884b64357f196c77523e015cb91e895
Julien <denneulin.julien@gmail.com>
2022-06-11 16:48:56 +0200

    produit affiché page acceuil

--- front/js/script.js
+++ front/js/script.js
@@ -1,35 +1,35 @@
+const articles = function(infos) {
+    
+    const section = document.getElementById("items");
+    
+    for (let i = 0;i < infos.length; i++ ){
+    let info = infos[i];
+
+    const lien = document.createElement("a");
+    lien.href = "./product.html?id=42";
+    section.appendChild(lien);
+    const article = document.createElement("article");
+    lien.appendChild(article);
+    const image = document.createElement("img");
+    image.src = info.imageUrl;
+    image.alt = info.altTxt;
+    article.appendChild(image);
+    const titre = document.createElement("h3");
+    titre.className = "productName";
+    let texte_titre = document.createTextNode(info.name);
+    titre.appendChild(texte_titre);
+    article.appendChild(titre);
+    const paragraphe = document.createElement("p");
+    paragraphe.className = "productDescription";
+    let texte_paragraphe = document.createTextNode(info.description);
+    paragraphe.appendChild(texte_paragraphe);
+    article.appendChild(paragraphe);
+}}
+
 fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();  
-    }   else {
-            console.log("Erreur réseau");
-    }})
-    .catch (function(e) {
-        console.log("Il y a un problème avec " + e);
+    .then(reponse => reponse.json())
+    .then(data => {
+        console.table(data);
+        articles(data);
     })
-    // création et insertion des éléments HTML !!
-const section = document.getElementById("items");
-const lien = document.createElement("a");
-lien.href = "./product.html?id=42";
-section.appendChild(lien);
-
-const article = document.createElement("article");
-lien.appendChild(article);
-
-const image = document.createElement("img");
-image.src = "";
-image.alt = "Lorem ipsum dolor sit amet, Kanap name1";
-article.appendChild(image);
-
-const titre = document.createElement("h3");
-titre.className = "productName";
-const texte_titre = document.createTextNode("Kanap name1");
-titre.appendChild(texte_titre);
-article.appendChild(titre);
-
-const paragraphe = document.createElement("p");
-paragraphe.className = "productDescription";
-const texte_paragraphe = document.createTextNode("Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.");
-paragraphe.appendChild(texte_paragraphe);
-article.appendChild(paragraphe);
+    .catch (e => console.log("Il y a un problème avec " + e));
